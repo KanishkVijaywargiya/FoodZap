fc085e33a227a786bc5453f24bb6d27007e41c3f
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2021-10-22 22:23:42 +0530

    json for recipe datas

---
+++ Shared/Model/FoodsCollections.swift
@@ -0,0 +1,34 @@
+//
+//  FoodsCollections.swift
+//  FoodZap
+//
+//  Created by KANISHK VIJAYWARGIYA on 20/10/21.
+//
+
+import SwiftUI
+
+//struct FoodsCollections: Identifiable {
+//    var id = UUID()
+//    var backgroundImg: String // food image
+//    var image1: String // more than one image of a food
+//    var image2: String // procedures images
+//
+//    var gradient1: Color
+//    var gradient2: Color
+//    var gradient3: Color
+//
+//    var title: String
+//    var description: String
+//    var chefName: String
+//    var procedure: String
+//}
+//
+//extension FoodsCollections {
+//    static var dummyData: [FoodsCollections] {
+//        [
+//
+//        ]
+//    }
+//}
+
+
