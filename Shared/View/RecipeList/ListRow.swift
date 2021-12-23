be808263e25889d4725f20e3c20622276d3472be
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2021-12-23 12:29:18 +0530

    UI modified for list rows

--- Shared/View/RecipeList/ListRow.swift
+++ Shared/View/RecipeList/ListRow.swift
@@ -16,7 +16,7 @@ struct ListRow: View {
             HStack(alignment: .top) {
                 ZStack {
                     Rectangle()
-                        .stroke(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), lineWidth: 10)
+                        .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 10)
                         .frame(width: 70, height: 70)
                         .cornerRadius(8)
                     
@@ -29,9 +29,10 @@ struct ListRow: View {
                                 .cornerRadius(8)
                         } else if imageLoader.errorMessage != nil {
                             Image("swiftuihandbook")
-                                .resizable().aspectRatio(contentMode: .fill)
+                                .resizable().aspectRatio(contentMode: .fit)
                                 .frame(width: 66, height: 66, alignment: .center)
                                 .mask(Rectangle())
+                                .background(Color.white)
                                 .cornerRadius(8)
                         } else {
                             ZStack {
@@ -49,22 +50,30 @@ struct ListRow: View {
                         imageLoader.fetch(for: fullListItem.backgroundImg)
                     }
                 }
-                .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 20)
+                .shadow(color: .black.opacity(0.3), radius: 3)
                 .padding(.top, 2)
                 
-                VStack(alignment: .leading) {
-                    Text(fullListItem.title)
-                        .font(.title)
-                        .bold()
-                        .lineLimit(2)
-                        .multilineTextAlignment(.leading)
+                VStack(alignment: .leading, spacing: 5) {
+                    HStack(alignment: .top) {
+                        Text(fullListItem.title)
+                            .font(.custom("American Typewriter", size: 18).bold())
+                            .lineLimit(1)
+                            .multilineTextAlignment(.leading)
                         
-                    Text(fullListItem.time)
-                        .font(.subheadline)
+                        Spacer()
+                        
+                        Text(fullListItem.time)
+                            .font(.subheadline)
+                            .padding(4)
+                            .background(BlurView(style: .light))
+                            .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous)
+                                        .stroke(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), lineWidth: 1))
+                            .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                    }
                     
                     Text(fullListItem.descriptions)
-                        .font(.footnote)
-                        .lineLimit(4)
+                        .font(.custom("American Typewriter", size: 12))
+                        .lineLimit(3)
                         .multilineTextAlignment(.leading)
                         .padding(.top, 1)
                 }
@@ -75,7 +84,7 @@ struct ListRow: View {
             
             Rectangle()
                 .frame(height: 1)
-                .foregroundColor(.black.opacity(0.3))
+                .foregroundColor(.black.opacity(0.1))
                 .padding(.top, 8)
         }
         .padding(.horizontal, 8)
